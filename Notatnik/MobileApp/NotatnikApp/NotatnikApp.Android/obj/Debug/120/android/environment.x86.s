2e392e3c808a7996af7ba53f1b9b48f6fdb0015d
EFox <elementalsfox@gmail.com>
2022-05-26 21:18:49 +0200

    Complete frontend

--- Notatnik/MobileApp/NotatnikApp/NotatnikApp.Android/obj/Debug/120/android/environment.x86.s
+++ Notatnik/MobileApp/NotatnikApp/NotatnikApp.Android/obj/Debug/120/android/environment.x86.s
@@ -84,7 +84,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"e6b46732-e63d-4b3c-8c53-4a5b26d44bb5"
+	.asciz	"f00715ef-b876-4871-9818-6e6d2816ec2a"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
