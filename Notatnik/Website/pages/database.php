b7c19955749e1e2a68b4f0c038725ec785835ec8
ymon1098 <ymon060708@gmail.com>
2022-05-27 21:08:13 +0200

    php questions

---
+++ Notatnik/Website/pages/database.php
@@ -0,0 +1,143 @@
+<?php
+
+    require_once('connect.php');
+
+    function decoder($value)
+    {
+        return htmlspecialchars($value);
+    }
+
+    function select($data, $table, $where)
+    {
+        $data = decoder($data);
+        $table = decoder($table);
+        $where = decoder($where);
+        try
+        {
+            global $host;
+            global $db_user;
+            global $db_password;
+            global $db_name;
+            $conn = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_password);
+            $stmt = $conn->prepare("SELECT $data FROM $table WHERE $where");
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e)
+        {
+            echo "Connection failed: " . $e->getMessage();
+            return $e->getMessage();
+        }
+        $conn = 0;
+        return $result;
+    }
+
+    function single_select($data, $table, $where)
+    {
+        $data = decoder($data);
+        $table = decoder($table);
+        $where = decoder($where);
+        try
+        {
+            global $host;
+            global $db_user;
+            global $db_password;
+            global $db_name;
+            $conn = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_password);
+            $stmt = $conn->prepare("SELECT $data FROM $table WHERE $where LIMIT 1");
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e)
+        {
+            echo "Connection failed: " . $e->getMessage();
+            return $e->getMessage();
+        }
+        $conn = 0;
+        return $result;
+    }
+
+    function insert($columns, $table, $what)
+    {
+        $columns = decoder($columns);
+        $table = decoder($table);
+        $what = decoder($what);
+        try
+        {
+            global $host;
+            global $db_user;
+            global $db_password;
+            global $db_name;
+            $conn = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_password);
+            $stmt = $conn->prepare("INSERT INTO $table($columns) VALUES $what");
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e)
+        {
+            echo "Connection failed: " . $e->getMessage();
+            return $e->getMessage();
+        }
+        $conn = 0;
+        return $result;
+    }
+
+    function update($what, $table, $where)
+    {
+        $what = decoder($what);
+        $table = decoder($table);
+        $where = decoder($where);
+        try
+        {
+            global $host;
+            global $db_user;
+            global $db_password;
+            global $db_name;
+            $conn = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_password);
+            $stmt = $conn->prepare("UPDATE $table SET $what WHERE $where");
+            $stmt->execute();
+            $result = $stmt->fetch();
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e)
+        {
+            echo "Connection failed: " . $e->getMessage();
+            return $e->getMessage();
+        }
+        $conn = 0;
+        return $result;
+    }
+
+    function delete($table, $where)
+    {
+        $table = decoder($table);
+        $where = decoder($where);
+        try
+        {
+            global $host;
+            global $db_user;
+            global $db_password;
+            global $db_name;
+            $conn = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_password);
+            $stmt = $conn->prepare("DELETE FROM $table WHERE $where");
+            $stmt->execute();
+            $result = $stmt->fetch();
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e)
+        {
+            echo "Connection failed: " . $e->getMessage();
+            return $e->getMessage();
+        }
+        $conn = 0;
+        return $result;
+    }
+
+    // delete("lists", "id=23");
+    // print_r(select('*', 'lists', '1'));
+    // print(select('*', 'lists', '1')[2]['name']);
+    
+?>
